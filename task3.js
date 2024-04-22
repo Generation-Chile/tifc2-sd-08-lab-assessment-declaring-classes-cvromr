f5915d6b341c03a4e375c1513a3f8e31b6355bae
cvromr <carolina.morales@genstudents.org>
2024-04-22 16:15:05 -0400

    Tarea Terminada

--- task3.js
+++ task3.js
@@ -1,8 +1,19 @@
 export class Player {
-  constructor() {
-  
+  constructor(name, level) {
+    this.name = name
+    this.level = level
+    this.info = function() {
+      console.log(this.name + " has reached Level " + this.level + "!")
+    }
+
+  }
   }
 
+  const player1 = new Player("Tara", 6);
+  console.log (player1.info())
 
-
-}
+  // //3. You have now been asked to include a method that will output a string to the console announcing a level up.
+  // * Modify the Player class so that it will accept a player name string and a level number in two separate arguments.
+  // * Then, define a shared object method `info()` that will print the following string, replacing the two placeholders:
+  //     * `<name> has reached Level <level>!`
+  //         * A player named **Tara** at level **6** should result in "`Tara has reached Level 6!`" printed to the console.
